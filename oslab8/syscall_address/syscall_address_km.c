645a30525ef82aa7a96a1c92079b80bb7486c312
sudoparsa <sudoparsa@gmail.com>
2022-09-03 09:37:27 -0700

    oslab8

--- oslab8/syscall_address/syscall_address_km.c
+++ oslab8/syscall_address/syscall_address_km.c
@@ -7,11 +7,16 @@ MODULE_AUTHOR("Parsa Hosseini");
 MODULE_DESCRIPTION("print syscall address");
 MODULE_VERSION("1.0");
 
-static unsigned long *syscall_table = (unsigned long *) 0xffffffffa8a002a0;
+static unsigned long *syscall_table = (unsigned long *) 0xffffffff836002a0;
 
 static int __init km_init(void) {
 	printk(KERN_INFO "Syscall table is at %lx\n", syscall_table[0]);
 	printk(KERN_INFO "Read syscall address is at %lx\n", syscall_table[__NR_read]);
+	printk(KERN_INFO "Write syscall address is at %lx\n", syscall_table[__NR_write]);
+	printk(KERN_INFO "Open syscall address is at %lx\n", syscall_table[__NR_open]);
+	printk(KERN_INFO "Close syscall address is at %lx\n", syscall_table[__NR_close]);
+	printk(KERN_INFO "Last syscall address is at %lx\n", syscall_table[NR_syscalls - 1]);
+	printk(KERN_INFO "Number of system calls in the system calls table: %d\n", NR_syscalls);
 	return 0;
 }
 
